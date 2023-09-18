9262c32444ba3be0bfd51f6687a158dbf02d84cc
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-09-18 23:30:01 +0200

    first fonction get

--- Server/Server.js
+++ Server/Server.js
@@ -1,7 +1,10 @@
-const express=require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
+app.get("/", (req, res) => {
+	res.send("Heloo Samoura");
+});
 
-app.listen('3001', ()=>{
-    console.log('server work')
-})
+app.listen('8000', () => {
+	console.log('server work');
+});
