27745111dca8b87fb065f27107587f06280446e8
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-09-19 21:34:00 +0200

    creat operation

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { useState, useEffect } from 'react';
 
 function App() {
 	const [users, setUsers] = useState([]);
+  const[name,setName]=useState("");
+  const[age,setAge]=useState("");
+  const[email,setEmail]=useState("");
 
 	useEffect(() => {
 		Axios.get('http://localhost:3004/users').then((res) => {
@@ -12,8 +15,24 @@ function App() {
 		});
 	}, []);
 
+  const createUser =()=>{
+    Axios.post('http://localhost:3004/AddUser',{
+      name:name ,
+      age: age,
+      email :email
+    })
+    .then((res) => {
+			console.log(" User created seccesfully :) ");
+      console.log(res.data)
+			
+		});
+  }
+
 	return (
+    
 		<>
+    
+
 			{users.map((user) => {
 				return (
 					<ul>
@@ -23,6 +42,18 @@ function App() {
 					</ul>
 				);
 			})}
+<div>
+
+<input type="text" placeholder='Name'  onChange={e=>setName(e.target.value)}/>
+
+<input type="number" placeholder='age' onChange={e=>setAge(e.target.value)}/>
+
+<input type="text" placeholder='Email' onChange={e=>setEmail(e.target.value)}/>
+
+<button onClick={createUser}>createUser</button>
+
+</div>
+
 		</>
 	);
 }
