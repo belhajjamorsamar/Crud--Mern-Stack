39ae6cfc71cd6e7b0ce8b9eb70e020645ed06bfb
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-09-19 17:03:54 +0200

    fetching data from the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,26 @@
 import './App.css';
-
-const users =[
-
-{id:1, name:"samar",age:20 , email:"belhajjamosamar@gmail.com"},
-{id:2, name:"majed",age:25 , email:"majedweldi@gmail.com"},
-{id:3, name:"anas",age:30 , email:"Anastoutou@gmail.com"},
-
-]
-
-
-
+import Axios from 'axios'
 
 function App() {
+
+Axios.get("http://localhost:3004/users")
+.then(res=>{
+  console.log(res.data)
+})
+
+
+
   return (
   <div>
-{users.map(user=>{
-  return(
+
     <ul>
 
-  <li>{user.name}</li>
-  <li>{user.age}</li>
-  <li>{user.email}</li>
+  <li>name:ssamar</li>
+  <li>age:22</li>
+  <li>email:dfghjkl</li>
   
   </ul>
 
-  )
-  
-
-})}
   </div>
 
   );
